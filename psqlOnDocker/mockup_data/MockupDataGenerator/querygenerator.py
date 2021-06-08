54804aedf4058714a6189e6e41e7302e96444fde
canta2899 <56071114+canta2899@users.noreply.github.com>
2021-06-08 10:38:34 +0200

    query generator

---
+++ psqlOnDocker/mockup_data/MockupDataGenerator/querygenerator.py
@@ -0,0 +1,11 @@
+def build(table, **kwargs):
+    keys = ','.join(kwargs.keys())
+    values = ','.join(kwargs.values())
+    return f"INSERT INTO {table}({keys}) VALUES ({values})"
+
+def make_sql(table, queries):
+    if not os.path.exists('sql'):
+        os.mkdir('sql')
+    with open(f"sql/{table}.sql", "a") as f:
+        f.writelines(queries)
+    print(f"Queries saved on file sql/{table}.sql")
